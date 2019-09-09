369d905fddff55317b044fa716e43809d52291f3
Heidi Dong <dong.heidi@gmail.com>
2019-09-08 19:59:37 -0700

    Add tags organization

--- archive.js
+++ archive.js
@@ -7,18 +7,18 @@ const CONFIG = {
 		{"id": "crucible", "name": "The Crucible", "tags": ["draft", "Berkeley Innovation", "design"]},
 		{"id": "paypal", "name": "PayPal", "tags": ["internship", "webdev", "frontend UI"]},
 		{"id": "mapd", "name": "MapD (now Omnisci)", "tags": ["draft", "Berkeley Innovation", "design"]},
-		{"id": "keewi", "name": "Keewi", "tags": ["internship", "webdev"]},
+		{"id": "keewi", "name": "Keewi", "tags": ["internship", "webdev", "mobile app", "design"]},
 		// {"id": "proclub", "name": "Programming Club", "tags": ["draft", "high school", "webdev"]},
 		{"id": "solarunion", "name": "SolarUnion", "tags": ["internship", "webdev"]},
-		{"id": "launch", "name": "Launch", "tags": ["high school", "Android"]},
+		{"id": "launch", "name": "Launch", "tags": ["high school", "mobile app"]},
 		{"id": "totalphase", "name": "Total Phase", "tags": ["internship"]},
-		{"id": "cgs", "name": "Threat Detected", "tags": ["high school", "game"]},
+		{"id": "cgs", "name": "Threat Detected", "tags": ["high school", "game", "graphic design"]},
 		{"id": "graphicdesign", "name": "Graphic Design", "tags": ["high school", "graphic design"]},
 		{"id": "profpics", "name": "Custom Profile Pictures", "tags": ["high school", "webdev"]},
 		{"id": "clubwebsites", "name": "Club Websites", "tags": ["draft", "high school", "webdev"]},
 		{"id": "willowlakebnb", "name": "Willow Lake", "tags": ["high school", "webdev"]},
-		{"id": "yogawithmiki", "name": "Yoga with Miki", "tags": ["middle school", "webdev"]},
-		{"id": "woodellcomputers", "name": "Woodell Computers", "tags": ["middle school", "webdev"]},
+		{"id": "yogawithmiki", "name": "Yoga with Miki", "tags": ["old", "middle school", "webdev"]},
+		{"id": "woodellcomputers", "name": "Woodell Computers", "tags": ["old", "middle school", "webdev"]},
 		{"id": "neopets", "name": "Neopets", "tags": ["draft", "old"]},
 	],
 };
@@ -27,7 +27,7 @@ let state = {
 	currentProject: "",
 }
 
-const projectList = CONFIG.projects.map(x => x.id);
+const projectList = CONFIG.projects.map(p => p.id);
 
 const tagTemplate = (tag) => `<span class="tag">${tag}</span>`;
 
@@ -52,6 +52,9 @@ const Page = {
 	},
 	renderContent: (markupStr) => {
 		document.getElementById("content").innerHTML = markupStr;
+		if (!state.currentProject || state.currentProject === "") {
+			TagLists.renderContent(TagLists.getContent());
+		}
 	}
 }
 
@@ -70,10 +73,37 @@ const Footer = {
 	}
 }
 
+const TagLists = {
+	getContent: () => {
+		let map = {};
+		CONFIG.projects.forEach(p => {
+			p.tags.forEach(tag => {
+				if (!map.hasOwnProperty(tag)) {
+					map[tag] = [];
+				}
+				if (!map[tag].includes(p)) {
+					map[tag].push(p);
+				}
+			});
+		});
+		return map;
+	},
+	renderContent: (map) => {
+		const tagsInAlphaOrder = Object.keys(map).sort();
+		const lists = tagsInAlphaOrder.map((tag) => {
+			return `<div>${marked(`${tag}\n${map[tag].map(p => `- [${p.name}](?id=${p.id})`).join('\n')}`)}</div>`;
+		});
+		console.log(lists)
+		document.getElementById("tagLists").innerHTML = lists.join("");
+	}
+}
+
 function render(state) {
 	Nav.renderContent(Nav.getContent(state.currentProject));
 	Page.getContent(state.currentProject).then(Page.renderContent);
-	Footer.renderContent(Footer.getContent(state.currentProject));
+	if (state.currentProject && state.currentProject !== "") {
+		Footer.renderContent(Footer.getContent(state.currentProject));
+	}
 }
 
 state.currentProject = new URL(window.location.href).searchParams.get("id");
